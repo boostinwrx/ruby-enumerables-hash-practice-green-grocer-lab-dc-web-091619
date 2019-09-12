7e29bc9438ee89ee9c6f6f85296ab9f6fbb4ed05
Antonio Abraham <tony.calablava@gmail.com>
2019-09-12 19:03:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -84,6 +84,7 @@ def apply_clearance(cart)
  cart = h
 end
 apply_clearance(consolidated_cart)
+<<<<<<< HEAD
 
 def checkout(cart,coupons)
 hash_cart = consolidate_cart(cart)
@@ -103,3 +104,17 @@ else
   end
 end
 checkout(cart,coupons)
+=======
+
+def checkout(array,coupons)
+hash_cart = consolidate_cart(array)
+applied_coupons = apply_coupons(hash_cart, 
+coupons)
+applied_discount = apply_clearance(applied_coupons)
+total = applied_discount.reduce(0) { |acc, (key,value) | 
+acc += value[:price] }
+total > 100 ? total * 0.9 : total 
+end
+checkout(cart,coupons
+
+>>>>>>> 8bc56305cb2a5295fa8a360b7675d2c64d13819c
